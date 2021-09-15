0334c68eea3d458fc37f7b52746469857c420cd9
Frank <fdccastilla@gmail.com>
2021-09-15 12:09:03 -0500

    ajustando hora

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const rawArray = (dataPlays) => {
 
 
 // script para ejecutar el evento cada 24 horas
-cron.schedule('1 5 12 * * *', () => {
+cron.schedule('1 10 12 * * *', () => {
     console.log('cron');
     scrape();    
 })
