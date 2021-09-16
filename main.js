6048bfd0c3735665a1e1f6d427d50d19a7c534a6
Frank <fdccastilla@gmail.com>
2021-09-16 13:05:27 -0500

    ajuste final api

--- main.js
+++ main.js
@@ -9,7 +9,31 @@ const PORT = process.env.PORT || 8080;
 
 const CronJob = require("cron").CronJob;
 
-global.arrayData = [];
+global.arrayData = [
+  "FC Kairat AC Omonoia 9:30 am Europa Conference League - Star+   ESPN Play",
+  "Real Betis Celtic FC 11:45 am Europa League - Star+",
+  "Lokomotiv Moskva O. Marseille 11:45 am Europa League - Star+     ESPN 3",
+  "FC Midtjylland PFC Ludogorets 11:45 am Europa League - Star+",
+  "Galatasaray Lazio 11:45 am Europa League - Star+",
+  "Rennes Tottenham 11:45 am Europa Conference League - Star+     ESPN Play",
+  "Rapid Wien Genk 11:45 am Europa League - Star+",
+  "Bayer Leverkusen Ferencvaros 11:45 am Europa League - Star+     ESPN 2 Andino",
+  "Crvena Zvezda SC Braga 11:45 am Europa League - Star+",
+  "GNK Dinamo Zagreb West Ham 11:45 am Europa League - Star+",
+  "PSV Eindhoven Real Sociedad 2:00 pm Europa League - Star+",
+  "AS Monaco Sturm Graz 2:00 pm Europa League - Star+     ESPN 3",
+  "Eintracht Frankfurt Fenerbahçe 2:00 pm Europa League - Star+",
+  "Olympiacos Antwerp 2:00 pm Europa League - Star+",
+  "Leicester City Napoli 2:00 pm Europa League - Star+     ESPN 2 Andino",
+  "AS Roma CSKA Sofia 2:00 pm Europa Conference League - Star+     ESPN Play",
+  "Brondby IFSparta Praha 2:00 pm Europa League - Star+",
+  "Rangers FCO. Lyonnais 2:00 pm Europa League - Star+",
+  "Caracas FCLala FC 3:00 pm Liga Futve - Gol TV Play",
+  "Monagas SCUCV 5:15 pm Liga Futve - Gol TV Play",
+  "Deportes Tolima La Equidad 5:30 pm Copa Bet Play DIMAYOR - Win Sports+",
+  "Deportivo Táchira Zulia 7:30 pm Liga Futve - Gol TV Play",
+  "Pereira Deportivo Pasto 8:00 pm Copa Bet Play DIMAYOR - Win Sports+"
+  ];
 
 const getDataRaw = (url) => {
   return fetch(url)
@@ -63,7 +87,7 @@ const main = () => {
   });
 
   let job = new CronJob(
-    " 50 10 * * *",
+    "5 9 * * *",
     () => {
       console.log("cron task working!");
       rawArray();
